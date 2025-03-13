5949cb3381a4068e522fbed50c3568e00627f25b
Björn Ritzl <bjorn.ritzl@gmail.com>
2025-03-13 15:25:03 +0100

    Added Thermal API etc

--- adpf/src/adpf.cpp
+++ adpf/src/adpf.cpp
@@ -12,10 +12,16 @@
 struct ADPFJNI
 {
 	jobject        m_JNI;
-	jmethodID      m_Initialize;
-	jmethodID      m_Finalize;
-	jmethodID      m_ReportWorkDuration;
-	jmethodID      m_UpdateTargetFPS;
+	jmethodID      m_HintInitialize;
+	jmethodID      m_HintFinalize;
+	jmethodID      m_HintReportWorkDuration;
+	jmethodID      m_HintUpdateTargetFPS;
+	jmethodID      m_ThermalInitialize;
+	jmethodID      m_ThermalFinalize;
+	jmethodID      m_ThermalGetStatus;
+	jmethodID      m_ThermalGetHeadroom;
+	bool           m_HintAvailable;
+	bool           m_ThermalAvailable;
 };
 
 static ADPFJNI g_ADPF;
@@ -35,22 +41,51 @@ static void CallVoidMethodLong(jobject instance, jmethodID method, const uint64_
 	env->CallVoidMethod(instance, method, arg1);
 }
 
+static int CallIntMethodLong(jobject instance, jmethodID method, const uint64_t arg1)
+{
+	dmAndroid::ThreadAttacher threadAttacher;
+	JNIEnv* env = threadAttacher.GetEnv();
+	jint result = env->CallIntMethod(instance, method, arg1);
+	return result;
+}
+
+static int CallIntMethodVoid(jobject instance, jmethodID method)
+{
+	dmAndroid::ThreadAttacher threadAttacher;
+	JNIEnv* env = threadAttacher.GetEnv();
+	jint result = env->CallIntMethod(instance, method);
+	return result;
+}
+
+static float CallFloatMethodInt(jobject instance, jmethodID method, const int arg1)
+{
+	dmAndroid::ThreadAttacher threadAttacher;
+	JNIEnv* env = threadAttacher.GetEnv();
+	jfloat result = env->CallFloatMethod(instance, method, arg1);
+	return result;
+}
+
+/**********************************
+ * PerformanceHintAPI
+ **********************************/
+
 static int PerformanceHint_Init(lua_State* L) {
-	DM_LUA_STACK_CHECK(L, 0);
+	DM_LUA_STACK_CHECK(L, 1);
 	uint64_t target_fps_nanos = (uint64_t)luaL_checknumber(L, 1);
-	dmLogInfo("PerformanceHint_Init %llu", target_fps_nanos);
-	CallVoidMethodLong(g_ADPF.m_JNI, g_ADPF.m_Initialize, target_fps_nanos);
-	return 0;
+	dmLogInfo("PerformanceHint_Init %lu", target_fps_nanos);
+	jint available = CallIntMethodLong(g_ADPF.m_JNI, g_ADPF.m_HintInitialize, target_fps_nanos);
+	lua_pushboolean(L, available);
+	g_ADPF.m_HintAvailable = available;
+	return 1;
 }
 
 static int PerformanceHint_UpdateTargetFPS(lua_State* L) {
 	DM_LUA_STACK_CHECK(L, 0);
 	uint64_t target_fps_nanos = (uint64_t)luaL_checknumber(L, 1);
-	dmLogInfo("PerformanceHint_UpdateTargetFPS %llu", target_fps_nanos);
-	CallVoidMethodLong(g_ADPF.m_JNI, g_ADPF.m_UpdateTargetFPS, target_fps_nanos);
+	dmLogInfo("PerformanceHint_UpdateTargetFPS %lu", target_fps_nanos);
+	CallVoidMethodLong(g_ADPF.m_JNI, g_ADPF.m_HintUpdateTargetFPS, target_fps_nanos);
 	return 0;
 }
-
 
 static const luaL_reg PerformanceHint_methods[] =
 {
@@ -58,6 +93,56 @@ static const luaL_reg PerformanceHint_methods[] =
 	{"update_target_fps", PerformanceHint_UpdateTargetFPS},
 	{0, 0}
 };
+
+
+/**********************************
+ * ThermalAPI
+ **********************************/
+
+static int Thermal_Init(lua_State* L) {
+	DM_LUA_STACK_CHECK(L, 1);
+	dmLogInfo("Thermal_Init");
+	int available = CallIntMethodVoid(g_ADPF.m_JNI, g_ADPF.m_ThermalInitialize);
+	lua_pushnumber(L, available);
+	g_ADPF.m_ThermalAvailable = available;
+	return 1;
+}
+
+static int Thermal_GetHeadroom(lua_State* L) {
+	DM_LUA_STACK_CHECK(L, 1);
+	int forecaseSeconds = luaL_checknumber(L, 1);
+	dmLogInfo("Thermal_GetHeadroom %d", forecaseSeconds);
+	float headroom = CallFloatMethodInt(g_ADPF.m_JNI, g_ADPF.m_ThermalGetHeadroom, forecaseSeconds);
+	lua_pushnumber(L, headroom);
+	return 1;
+}
+
+static int Thermal_GetStatus(lua_State* L) {
+	DM_LUA_STACK_CHECK(L, 1);
+	dmLogInfo("Thermal_GetStatus");
+	int status = CallIntMethodVoid(g_ADPF.m_JNI, g_ADPF.m_ThermalGetStatus);
+	lua_pushnumber(L, status);
+	return 1;
+}
+
+static const luaL_reg Thermal_methods[] =
+{
+	{"init", Thermal_Init},
+	{"get_headroom", Thermal_GetHeadroom},
+	{"get_status", Thermal_GetStatus},
+	{0, 0}
+};
+
+
+
+
+static void lua_setfieldstringint(lua_State* L, const char* key, uint32_t value)
+{
+    int top = lua_gettop(L);
+    lua_pushnumber(L, value);
+    lua_setfield(L, -2, key);
+    assert(top == lua_gettop(L));
+}
 
 static void LuaInit(lua_State* L) {
 	DM_LUA_STACK_CHECK(L, 0);
@@ -79,6 +164,19 @@ static void LuaInit(lua_State* L) {
 	luaL_register(L, NULL, PerformanceHint_methods);
 	lua_settable(L, -3);
 
+	// create thermal subtable
+	lua_pushstring(L, "thermal");
+	lua_newtable(L);
+	luaL_register(L, NULL, Thermal_methods);
+	lua_setfieldstringint(L, "THERMAL_STATUS_NONE", 0);
+	lua_setfieldstringint(L, "THERMAL_STATUS_LIGHT", 1);
+	lua_setfieldstringint(L, "THERMAL_STATUS_MODERATE", 2);
+	lua_setfieldstringint(L, "THERMAL_STATUS_SEVERE", 3);
+	lua_setfieldstringint(L, "THERMAL_STATUS_CRITICAL", 4);
+	lua_setfieldstringint(L, "THERMAL_STATUS_EMERGENCY", 5);
+	lua_setfieldstringint(L, "THERMAL_STATUS_SHUTDOWN", 6);
+	lua_settable(L, -3);
+
 	lua_pop(L, 1); // pop "adpf" global table
 }
 
@@ -92,10 +190,15 @@ static void JavaInit() {
 	g_ADPF.m_JNI = env->NewGlobalRef(env->NewObject(cls, jni_constructor, threadAttacher.GetActivity()->clazz));
 
 	// get function references
-	g_ADPF.m_Initialize 		= env->GetMethodID(cls, "initialize", "(J)V");
-	g_ADPF.m_Finalize 			= env->GetMethodID(cls, "finalize", "()V");
-	g_ADPF.m_ReportWorkDuration = env->GetMethodID(cls, "reportWorkDuration", "()V");
-	g_ADPF.m_UpdateTargetFPS 	= env->GetMethodID(cls, "updateTargetFPS", "(J)V");
+	g_ADPF.m_HintInitialize 		= env->GetMethodID(cls, "hintInitialize", "(J)I");
+	g_ADPF.m_HintFinalize 			= env->GetMethodID(cls, "hintFinalize", "()V");
+	g_ADPF.m_HintReportWorkDuration = env->GetMethodID(cls, "hintReportWorkDuration", "()V");
+	g_ADPF.m_HintUpdateTargetFPS 	= env->GetMethodID(cls, "hintUpdateTargetFPS", "(J)V");
+
+	g_ADPF.m_ThermalInitialize 		= env->GetMethodID(cls, "thermalInitialize", "()I");
+	g_ADPF.m_ThermalFinalize 		= env->GetMethodID(cls, "thermalFinalize", "()V");
+	g_ADPF.m_ThermalGetHeadroom 	= env->GetMethodID(cls, "thermalGetHeadroom", "(I)F");
+	g_ADPF.m_ThermalGetStatus	 	= env->GetMethodID(cls, "thermalGetStatus", "()I");
 }
 
 #endif
@@ -121,7 +224,10 @@ dmExtension::Result AppFinalizeADPFExtension(dmExtension::AppParams* params) {
 
 dmExtension::Result FinalizeADPFExtension(dmExtension::Params* params) {
 #if defined(DM_PLATFORM_ANDROID)
-	CallVoidMethod(g_ADPF.m_JNI, g_ADPF.m_Finalize);
+	CallVoidMethod(g_ADPF.m_JNI, g_ADPF.m_HintFinalize);
+	CallVoidMethod(g_ADPF.m_JNI, g_ADPF.m_ThermalFinalize);
+	g_ADPF.m_HintAvailable = 0;
+	g_ADPF.m_ThermalAvailable = 0;
 #endif
 	return dmExtension::RESULT_OK;
 }
@@ -129,7 +235,10 @@ dmExtension::Result FinalizeADPFExtension(dmExtension::Params* params) {
 // https://developer.android.com/ndk/reference/group/a-performance-hint
 dmExtension::Result UpdateADPFExtension(dmExtension::Params* params) {
 #if defined(DM_PLATFORM_ANDROID)
-	CallVoidMethod(g_ADPF.m_JNI, g_ADPF.m_ReportWorkDuration);
+	if (g_ADPF.m_HintAvailable)
+	{
+		CallVoidMethod(g_ADPF.m_JNI, g_ADPF.m_HintReportWorkDuration);
+	}
 #endif
 	return dmExtension::RESULT_OK;
 }
--- adpf/src/com/defold/adpf/ADPFJNI.java
+++ adpf/src/com/defold/adpf/ADPFJNI.java
@@ -2,6 +2,7 @@ package com.defold.adpf;
 
 import android.os.PerformanceHintManager;
 import android.os.PerformanceHintManager.Session;
+import android.os.PowerManager;
 import android.content.Context;
 import android.app.Activity;
 import android.util.Log;
@@ -11,45 +12,48 @@ public class ADPFJNI {
 
     private Activity activity;
     private PerformanceHintManager.Session hintSession;
+    private PowerManager powerManager;
     private long timestamp;
 
     public ADPFJNI(Activity activity) {
         this.activity = activity;
     }
 
-    public void initialize(long targetFpsNanos) {
+    public int hintInitialize(long targetFpsNanos) {
         if (this.hintSession != null) {
-            Log.e(TAG, "initialize() - Already initialized");
-            return;
+            Log.e(TAG, "hintInitialize() - Already initialized");
+            return 1;
         }
 
         int[] tids = {
             android.os.Process.myTid()
         };
-        Log.d(TAG, "initialize() target fps: " + targetFpsNanos + "ns tid: " + tids[0]);
+        Log.d(TAG, "hintInitialize() target fps: " + targetFpsNanos + "ns tid: " + tids[0]);
         PerformanceHintManager performanceHintManager = (PerformanceHintManager)this.activity.getSystemService(Context.PERFORMANCE_HINT_SERVICE);
         this.hintSession = performanceHintManager.createHintSession(tids, targetFpsNanos);
         this.timestamp = System.nanoTime();
 
         if (this.hintSession != null) {
-            Log.d(TAG, "initialize() success!");
+            Log.d(TAG, "hintInitialize() success!");
+            return 1;
         }
         else {
-            Log.d(TAG, "initialize() failed!");
+            Log.d(TAG, "hintInitialize() failed!");
+            return 0;
         }
     }
 
-    public void finalize() {
+    public void hintFinalize() {
         if (this.hintSession != null) {
-            Log.d(TAG, "finalize()");
+            Log.d(TAG, "hintFinalize()");
             this.hintSession.close();
             this.hintSession = null;
         }
     }
 
-    public void reportWorkDuration() {
+    public void hintReportWorkDuration() {
         if (this.hintSession == null) {
-            Log.e(TAG, "reportWorkDuration() - Not initialized");
+            Log.e(TAG, "hintReportWorkDuration() - Not initialized");
             return;
         }
         long now = System.nanoTime();
@@ -59,12 +63,62 @@ public class ADPFJNI {
     }
 
 
-    public void updateTargetFPS(long targetFpsNanos) {
+    public void hintUpdateTargetFPS(long targetFpsNanos) {
         if (this.hintSession == null) {
-            Log.e(TAG, "updateTargetFPS() - Not initialized");
+            Log.e(TAG, "hintUpdateTargetFPS() - Not initialized");
             return;
         }
-        Log.d(TAG, "updateTargetFPS()");
+        Log.d(TAG, "hintUpdateTargetFPS()");
         this.hintSession.updateTargetWorkDuration(targetFpsNanos);
     }
+
+
+    public int thermalInitialize() {
+        if (this.powerManager != null) {
+            Log.e(TAG, "thermalInitialize() - Already initialized");
+            return 1;
+        }
+
+        this.powerManager = (PowerManager)this.activity.getSystemService(Context.POWER_SERVICE);
+        if (this.powerManager == null) {
+            Log.d(TAG, "thermalInitialize() failed!");
+            return 0;
+        }
+
+        float headroom = this.powerManager.getThermalHeadroom(1);
+        if (headroom == 0.0 || Float.isNaN(headroom)) {
+            Log.d(TAG, "thermalInitialize() failed!");
+            this.powerManager = null;
+            return 0;
+        }
+
+        Log.d(TAG, "thermalInitialize() success!");
+        return 1;
+    }
+
+    public void thermalFinalize() {
+        if (this.powerManager != null) {
+            Log.d(TAG, "thermalFinalize()");
+            this.powerManager = null;
+        }
+    }
+
+    public float thermalGetHeadroom(int forecastSeconds) {
+        if (this.powerManager == null) {
+            Log.e(TAG, "thermalGetHeadroom() - Not initialized");
+            return -1;
+        }
+        Log.d(TAG, "thermalGetHeadroom()");
+        return this.powerManager.getThermalHeadroom(forecastSeconds);
+    }
+
+    public int thermalGetStatus() {
+        if (this.powerManager == null) {
+            Log.e(TAG, "thermalGetStatus() - Not initialized");
+            return -1;
+        }
+        Log.d(TAG, "thermalGetStatus()");
+        return this.powerManager.getCurrentThermalStatus();
+    }
+
 }
